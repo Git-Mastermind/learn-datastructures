77861250156e69c73f079c7ee898d9e8ec1a4b28
Git-Mastermind <eshan-jha@outlook.com>
2026-05-30 16:35:26 -0700

    rewrite ArrayList class to make it more simple, make constructor only take in size for the Array. make add, size, get, emove, indexOf and addAtEnd

--- ArrayListImplementation/ArrayList.java
+++ ArrayListImplementation/ArrayList.java
@@ -2,84 +2,26 @@ package com.datastructures.ArrayListImplementation;
 
 public class ArrayList {
 
-    String[] array;
+    public String[] array;
 
-    public ArrayList(String[] array) {
-        this.array = array;
+    public ArrayList(int size) {
+        array = new String[size];
     }
 
-    public String[] append(String valueToAppend) {
-        String[] newArray = new String[array.length + 1];
-
-        for (int i = 0; i < array.length; i++) {
-            newArray[i] = array[i];
-        }
-        newArray[array.length] = valueToAppend;
-        return newArray;
-        }
-
-    public void delete(String valueToErase) {
-        String[] newArray = new String[array.length - 1];
-
-        int indexOfValueToErase = -1;
-
-        for (int i = 0; i <= newArray.length; i++) {
-            if (indexOfValueToErase != -1) {
-                if (array[i] != valueToErase) {
-                    newArray[i - 1] = array[i];
-                }
-            }
-            else {
-                if (array[i] == valueToErase) {
-                    indexOfValueToErase = i + 1;
-                }
-                else {
-                    newArray[i] = array[i];
-                }
-            }
-        }
-        
-        turnArrayToStringRepresentation(newArray);
+    public void add(int index, String value) {
+        array[index] = value;
     }
 
-    public void turnArrayToStringRepresentation(String[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
-    }
-
-    public boolean isEmpty() {
-        int length = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != null) {
-                length++;
-            }
-        }
-
-        if (length == 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    public boolean contains(String value) {
-        
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == value) {
-                return true;
-            }
-        }
-        return false;
+    public int size() {
+        return array.length;
     }
 
     public String get(int index) {
-        if (index > array.length - 1) {
-            return "IndexOutOfBoundException!";
-        }
         return array[index];
+    }
+
+    public void remove(int index) {
+        array[index] = null;
     }
 
     public int indexOf(String value) {
@@ -91,16 +33,111 @@ public class ArrayList {
         return -1;
     }
 
-    public int lastIndexOf(String value) {
-        int index = -1;
-
+    public void addAtEnd(String value) {
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == value) {
-                index = i;
+            if (array[i] == null) {
+                array[i] = value;
             }
         }
-        return index;
     }
+
+
+
+
+
+    // public String[] append(String valueToAppend) {
+    //     String[] newArray = new String[array.length + 1];
+
+    //     for (int i = 0; i < array.length; i++) {
+    //         newArray[i] = array[i];
+    //     }
+    //     newArray[array.length] = valueToAppend;
+    //     return newArray;
+    //     }
+
+    // public void delete(String valueToErase) {
+    //     String[] newArray = new String[array.length - 1];
+
+    //     int indexOfValueToErase = -1;
+
+    //     for (int i = 0; i <= newArray.length; i++) {
+    //         if (indexOfValueToErase != -1) {
+    //             if (array[i] != valueToErase) {
+    //                 newArray[i - 1] = array[i];
+    //             }
+    //         }
+    //         else {
+    //             if (array[i] == valueToErase) {
+    //                 indexOfValueToErase = i + 1;
+    //             }
+    //             else {
+    //                 newArray[i] = array[i];
+    //             }
+    //         }
+    //     }
+        
+    //     turnArrayToStringRepresentation(newArray);
+    // }
+
+    // public void turnArrayToStringRepresentation(String[] array) {
+    //     for (int i = 0; i < array.length; i++) {
+    //         System.out.println(array[i]);
+    //     }
+    // }
+
+    // public boolean isEmpty() {
+    //     int length = 0;
+
+    //     for (int i = 0; i < array.length; i++) {
+    //         if (array[i] != null) {
+    //             length++;
+    //         }
+    //     }
+
+    //     if (length == 0) {
+    //         return true;
+    //     }
+    //     else {
+    //         return false;
+    //     }
+    // }
+
+    // public boolean contains(String value) {
+        
+    //     for (int i = 0; i < array.length; i++) {
+    //         if (array[i] == value) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // public String get(int index) {
+    //     if (index > array.length - 1) {
+    //         return "IndexOutOfBoundException!";
+    //     }
+    //     return array[index];
+    // }
+
+    // public int indexOf(String value) {
+    //     for (int i = 0; i < array.length; i++) {
+    //         if (array[i] == value) {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+    // public int lastIndexOf(String value) {
+    //     int index = -1;
+
+    //     for (int i = 0; i < array.length; i++) {
+    //         if (array[i] == value) {
+    //             index = i;
+    //         }
+    //     }
+    //     return index;
+    // }
     
     
 }
--- ArrayListImplementation/ArrayListTester.java
+++ ArrayListImplementation/ArrayListTester.java
@@ -3,13 +3,41 @@ package com.datastructures.ArrayListImplementation;
 public class ArrayListTester {
 
     public static void main(String[] args) {
+        //Testing the constructor
+        ArrayList constructorTestList = new ArrayList(3);
+        //todo: 
+        System.out.println(constructorTestList.size()==3);
 
-        String[] letterArray = {"a", "b", "c", "d", "e", "d"};
-        ArrayList tester = new ArrayList(letterArray);
+        //Testing add method
+        ArrayList addTestList = new ArrayList(3);
+        addTestList.add(0, "zero");
+        addTestList.add(1, "one");
+        //todo: 
+        System.out.println(addTestList.get(0).equals("zero"));
+
+        //Testing remove method
+        ArrayList deleteTestList = new ArrayList(3);
+        deleteTestList.remove(1);
+        System.out.println(deleteTestList.get(1)==null);
+
+        //Testing indexOf method
+        ArrayList indexOfTestList = new ArrayList(3);
+        indexOfTestList.add(0, "zero");
+        System.out.println(indexOfTestList.indexOf("zero")==0);
+
+        //Testing addAtEnd method
+        ArrayList addAtEndTestList = new ArrayList(5);
+        addAtEndTestList.addAtEnd("two");
+        System.out.println(addAtEndTestList.get(2).equals("two"));
+
+        //
         
-        System.out.println(tester.lastIndexOf("h"));
+
+
+
+
     }
-    
+  
 
     
 }
