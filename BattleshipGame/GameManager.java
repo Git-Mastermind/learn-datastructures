68b4c294c148d4826578fd32e1e3d58d1f4df3b3
Git-Mastermind <eshan-jha@outlook.com>
2026-06-03 21:22:33 -0700

    remove SinkTheStartup and make BattleshipGame folder. Add MainGame, GameManager and Battleships

---
+++ BattleshipGame/GameManager.java
@@ -0,0 +1,10 @@
+package com.datastructures.BattleshipGame;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class GameManager {
+
+    List<Battleships> battleships = new ArrayList<>();
+
+}
