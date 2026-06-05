15a16c13c01b301a22bb64c0972c4492dfce8d1c
Git-Mastermind <eshan-jha@outlook.com>
2026-06-04 17:27:21 -0700

    make all three battleships objects and continue to make position randomizer

--- BattleshipGame/Battleships.java
+++ BattleshipGame/Battleships.java
@@ -7,9 +7,13 @@ public class Battleships {
     String name;
     int length;
     List<String> position = new ArrayList<>();
+    private static final String STRING_POSITION_OPTIONS = "abcdef";
+    private static final int INTEGER_POSITION_OPTIONS = 12345;
 
     public Battleships(String battleshipName, int battleshipLength) {
         this.name = battleshipName;
         this.length = battleshipLength;
     }
+
+    
 }
--- BattleshipGame/GameManager.java
+++ BattleshipGame/GameManager.java
@@ -9,6 +9,15 @@ public class GameManager {
 
     public GameManager() {
         Battleships battleship1 = new Battleships("Aircraft Carrier", 5);
+        battleships.add(battleship1);
+
+        Battleships battleship2 = new Battleships("Destroyer", 3);
+        battleships.add(battleship2);
+
+        Battleships battleship3 = new Battleships("Boat", 2);
+        battleships.add(battleship3);
+
+        
     }
 
 }
