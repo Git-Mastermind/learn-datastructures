b72632831a28f158a778eeacc66e3667bb435996
Git-Mastermind <eshan-jha@outlook.com>
2026-06-09 18:43:56 -0700

    modify GameManager and Battleship classes

--- BattleshipGame/GameManager.java
+++ BattleshipGame/GameManager.java
@@ -2,10 +2,14 @@ package com.datastructures.BattleshipGame;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GameManager {
 
     List<Battleships> battleships = new ArrayList<>();
+    Random random = new Random();
+    private List<String> stringOptionsForPosition = new ArrayList<>(List.of("a", "b", "c", "d", "e", "f"));
+    private List<String> numberOptionsForPosition = new ArrayList<>(List.of("1", "2", "3", "4", "5", "6"));
 
     public GameManager() {
         Battleships battleship1 = new Battleships("Aircraft Carrier", 5);
@@ -16,8 +20,14 @@ public class GameManager {
 
         Battleships battleship3 = new Battleships("Boat", 2);
         battleships.add(battleship3);
-
-        
     }
+
+    public void randomizePositions() {
+        List<String> position = new ArrayList<>();
+        for (int i = 0; i < battleships.size(); i++) {
+            int randomStringIndex = random.nextInt(stringOptionsForPosition.size());
+            String randomString = stringOptionsForPosition.get(randomStringIndex);
+        }
+    }  
 
 }
