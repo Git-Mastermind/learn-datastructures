056652da540ac25ec5d98108b4c16964d96c8265
Git-Mastermind <eshan-jha@outlook.com>
2026-06-15 19:15:13 -0700

    finish building CodeMagnets, tested - works

--- JavaBookExercises/CodeMagnets.java
+++ JavaBookExercises/CodeMagnets.java
@@ -1,5 +1,34 @@
 package com.datastructures.JavaBookExercises;
 
+import java.util.ArrayList;
+
 public class CodeMagnets {
-    
+    public static ArrayList<String> arrayList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        arrayList.add(0, "zero");
+        arrayList.add(1, "one");
+        arrayList.add(2, "two");
+        arrayList.add(3, "three");
+        printList(arrayList);
+
+        if (arrayList.contains("three")) {
+            arrayList.add("four");
+        }
+        printList(arrayList);
+        arrayList.remove("two");
+
+        if (arrayList.indexOf("four") != 4) {
+            arrayList.add("4.2");
+        }
+        printList(arrayList);
+        printList(arrayList);
+    }
+
+    public static void printList(ArrayList<String> list) {
+        for (String element : list) {
+            System.out.println(element + " ");
+        }
+        System.out.println();
+    }
 }
