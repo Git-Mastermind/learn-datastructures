6f1365b8984a55bd1e7855f5c57a2c98f5ee9681
Git-Mastermind <eshan-jha@outlook.com>
2026-07-24 12:58:22 -0700

    test Stack methods

--- BinaryTrees/Tester.java
+++ BinaryTrees/Tester.java
@@ -1,12 +1,17 @@
 package com.datastructures.BinaryTrees;
 import com.datastructures.Queue.Queue;
+import com.datastructures.Stack.Stack;
 
 public class Tester {
     public static void main(String[] args) {
         Queue<Integer> queue = new Queue<Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
         
+        // stack.append(1);
+        // stack.append(2);
+        // stack.append(3);
 
-        System.out.println(queue.isEmpty());
+        System.out.println(stack.isEmpty());    
     }
     
     
