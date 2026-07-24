cdfa0be081142ecf944883a546613867135917c2
Git-Mastermind <eshan-jha@outlook.com>
2026-07-24 12:08:21 -0700

    make Queue and add some methods

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -1,5 +1,16 @@
 package com.datastructures.Queue;
+import java.util.List;
+import java.util.ArrayList;
 
-public class Queue {
-    
+
+public class Queue<T> {
+    List<T> queue = new ArrayList<>();
+
+    public void append(T value) {
+        queue.add(value);
+    }
+
+    public T peek() {
+        return queue.get(-1);
+    }
 }
