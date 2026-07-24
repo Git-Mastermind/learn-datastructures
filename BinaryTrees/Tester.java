da4c6d70ff6e2e725a19a1219056386fa092d32c
Git-Mastermind <eshan-jha@outlook.com>
2026-07-24 12:47:32 -0700

    modify Tester

--- BinaryTrees/Tester.java
+++ BinaryTrees/Tester.java
@@ -4,13 +4,9 @@ import com.datastructures.Queue.Queue;
 public class Tester {
     public static void main(String[] args) {
         Queue<Integer> queue = new Queue<Integer>();
-        queue.append(1);
-        queue.append(2);
-        queue.append(3);
-        queue.append(4);
-        queue.append(5);
+        
 
-        System.out.println(queue.poll());
+        System.out.println(queue.isEmpty());
     }
     
     
