3cf431e84bf08cbe25f7e4b5f0d17c44d72fbdf5
Git-Mastermind <eshan-jha@outlook.com>
2026-07-24 12:58:44 -0700

    make stacks and implement couple of methods

---
+++ Stack/Stack.java
@@ -0,0 +1,27 @@
+package com.datastructures.Stack;
+import java.util.List;
+import java.util.ArrayList;
+public class Stack<T> {
+    List<T> stack = new ArrayList<>();
+
+    public void append(T value) {
+        stack.add(value);
+    }
+
+    public boolean isEmpty() {
+        return stack.size()==0?true:false;
+    }
+
+    public T peek() {
+        return stack.get(stack.size() - 1);
+    }
+
+    public T pop() {
+        T valueToReturn = stack.get(stack.size() - 1);
+        stack.remove(stack.size() - 1);
+        return valueToReturn;
+    }
+
+    
+
+}
