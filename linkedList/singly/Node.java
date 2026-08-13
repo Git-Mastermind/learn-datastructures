2efe87378cd1996bbe9abc9677ce66de761bb92d
Git-Mastermind <eshan-jha@outlook.com>
2026-08-13 16:35:39 -0700

    make constructor for Node

--- linkedList/singly/Node.java
+++ linkedList/singly/Node.java
@@ -1,5 +1,14 @@
 package com.datastructures.linkedlist.singly;
 
-public class Node {
+public class Node<T> {
+    T value;
+    Node<T> next;
+
+    
+    public Node(T value, Node<T> next) {
+        this.value = value;
+        this.next = next;
+    }
+
     
 }
