1f00c02ee59c26ab72e7ebb410bb77e2d4ea23f2
Git-Mastermind <eshan-jha@outlook.com>
2026-08-12 20:15:09 -0700

    make string reverser or unreverser (depends on how you want to use it) using Stacks in java

---
+++ StackAgain/ReverseString.java
@@ -0,0 +1,19 @@
+package com.datastructures.StackAgain;
+
+public class ReverseString {
+    private Stack<Character> stack;
+    private String reversedString = "";
+
+    public ReverseString() {
+        stack = new Stack<>();
+    }
+    public String reverseString(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+        for (int i = 0; i < str.length(); i++) {
+            reversedString = str.charAt(i) + reversedString;
+        }
+        return reversedString;
+    }
+}
