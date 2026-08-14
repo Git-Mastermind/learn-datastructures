12997395184a63761c593736f865349cb5062cbc
Git-Mastermind <eshan-jha@outlook.com>
2026-08-13 17:13:27 -0700

    make prepend method

---
+++ tests/linkedlisttests/singlytests/SinglyLinkedListTester.java
@@ -0,0 +1,14 @@
+package com.datastructures.tests.linkedlisttests.singlytests;
+import com.datastructures.linkedlist.singly.SinglyLinkedList;
+import com.datastructures.linkedlist.singly.Node;
+
+public class SinglyLinkedListTester {
+    public static void main(String[] args) {
+        Node<String> head = new Node<>("A", null);
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<>(head);
+        linkedList.append("B");
+        linkedList.prepend("C");
+        linkedList.formattedPrint();
+
+    }
+}
